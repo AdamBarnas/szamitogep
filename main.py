2b0532d97c1b644dd4b5c9ef1da837959656bcdb
AdamBarnas <101982972+AdamBarnas@users.noreply.github.com>
2023-04-26 15:43:11 +0200

    szkielet main

---
+++ main.py
@@ -0,0 +1,13 @@
+# TODO definiowanie problemu
+
+# TODO redukcja macierzy
+
+# TODO wyznaczamy zera niezależne i niezależne
+#  a jeśli jest ich n to mamy rozwiązanie optymalne
+
+# TODO sprawdzenie liczby zer zależnych
+
+# TODO wykreślanie jeśli n zer niezależnych to koniec
+
+# TODO powiekszanie zbioeu libcz niezależnych
+
