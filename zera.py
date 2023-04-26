1562385ad091417f00ea76eac67f6d99754e7f77
AdamBarnas <101982972+AdamBarnas@users.noreply.github.com>
2023-04-26 23:05:16 +0200

    poprawka macierzy

--- zera.py
+++ zera.py
@@ -4,9 +4,18 @@ macierz = [[0, 1, 0, 1, 0, 1],
            [1, 0, 1, 1, 1, 1],
            [0, 1, 1, 1, 1, 1],
            [1, 0, 1, 1, 1, 1]]
+
+macierz2 = [[1, 1, 0, 1, 0, 1],
+            [1, 1, 1, 0, 1, 0],
+            [0, 1, 1, 1, 1, 1],
+            [1, 0, 1, 1, 1, 1],
+            [0, 1, 0, 1, 1, 1],
+            [1, 0, 1, 0, 1, 1]]
+
+
 def zera(macierz):
     rozmiar = len(macierz[0])
-    pomocnicza = [[0 for _ in range(rozmiar)]for _ in range(rozmiar)]
+    pomocnicza = [[0 for _ in range(rozmiar)] for _ in range(rozmiar)]
     row = [0 for _ in range(rozmiar)]
     col = [0 for _ in range(rozmiar)]
     for i in range(rozmiar):
@@ -15,15 +24,16 @@ def zera(macierz):
                 pomocnicza[i][j] = 1
                 row[i] += 1
                 col[j] += 1
-    minimum = rozmiar*2
     idx = [-1, -1]
     wybrane = [set(), set()]
     niezalezne = []
     for _ in range(rozmiar):
+        minimum = rozmiar * 2
         changed = False
         for i in range(rozmiar):
             for j in range(rozmiar):
                 if pomocnicza[i][j] == 1 and row[i] + col[j] < minimum and i not in wybrane[0] and j not in wybrane[1]:
+                    minimum = row[i] + col[j]
                     idx = [i, j]
                     changed = True
         if not changed:
@@ -34,4 +44,5 @@ def zera(macierz):
     return niezalezne
 
 
-
+print(zera(macierz))
+print(zera(macierz2))
