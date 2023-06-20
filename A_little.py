98c6152f0dc1a3cd7aa641f8f72f4f72f4e3d5b1
AdamBarnas <101982972+AdamBarnas@users.noreply.github.com>
2023-06-20 22:52:26 +0200

    Update A_little.py

--- A_little.py
+++ A_little.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from redukcja import reduction
 import optymistyczne_wyznaczenie_odc as owo
 from zabranianie_podcykli import zabranianie
@@ -5,27 +7,25 @@ from kryteria_zamykania import rozmiar, kryteria_zamykania
 import numpy as np
 
 class Podproblem:
-    def init(self, macierz, ograniczenie, typ, LB=float("inf"), parent=None, children=[]):
+    def init(self, macierz: list[list[int]], LB=float("inf"), wybrane=[], zabronione=[], typ=True):
         self.M = macierz
-        self.ograniczenie = ograniczenie
+        self.zabronione = zabronione
         self.LB = LB
-        self.parent = parent
-        self.pochodne = children
+        self.wybrane = wybrane
         self.typ = typ
 
-def krok3(P: Podproblem, old_LB, best):
+def krok3(P: Podproblem):
     if P.typ:
         for i in range(len(P.M)):
-            P.M[P.ograniczenie[0]][i] = float("inf")
+            P.M[P.wybrane[-1][0]][i] = float("inf")
         for i in range(len(P.M)):
-            P.M[i][P.ograniczenie[1]] = float("inf")
-        P.M[P.ograniczenie[1]][P.ograniczenie[0]] = float("inf")
-        zredukowana, LB = reduction(P.M)
+            P.M[i][P.wybrane[-1][1]] = float("inf")
+        P.M[P.wybrane[-1][1]][P.wybrane[-1][0]] = float("inf")
     else:
-        P.M[P.ograniczenie[1]][P.ograniczenie[0]] = float("inf")
+        P.M[P.zabronione[-1][1]][P.zabronione[-1][0]] = float("inf")
+    P.M, P.LB = reduction(P.M)
+    return P    # zmienione parametry podproblemu
 
-    # sprawdzenie dolnego ograniczenia
-    return "PP"    #"""kolejny problem"""
 
 
 def little(M):
@@ -33,14 +33,36 @@ def little(M):
     zredukowana, LB = reduction(M)
     LP = []  # listapodproblemow
     #optymistyczne wyznaczanie odcinka
-
     odcinek, koszt = owo.optymistyczne_wyznaczanie_odc(zredukowana)
-    LP.append(Podproblem(zredukowana, odcinek, False, LB))
-    LP.append(Podproblem(zredukowana, odcinek, True, LB))
+    LP.append(Podproblem(zredukowana, LB, wybrane=[odcinek], typ=True))
+    LP.append(Podproblem(zredukowana, LB, zabronione=[odcinek], typ=False))
     while len(LP) > 0:
-        podproblem = LP.pop()
-        PP = krok3(podproblem, LB, best)
-        LP.append(PP)
+        podproblem = LP.pop(0)
+        podproblem = krok3(podproblem)
+        KZ = kryteria_zamykania(podproblem.M, LB, podproblem.LB)
+        if KZ == "KZ0":
+            odcinek, koszt = owo.optymistyczne_wyznaczanie_odc(podproblem.M)
+            wybrane = podproblem.wybrane
+            zabronione = podproblem.zabronione
+            LP.append(Podproblem(podproblem.M, LB, wybrane + [odcinek], zabronione, typ=True))
+            LP.append(Podproblem(podproblem.M, LB, wybrane, zabronione + [odcinek], typ=False))
+        elif KZ == "KZ2":
+            pass
+        elif KZ == "KZ3":
+            pass
+        else:
+            print("error")
+            break
+
+        # LP.append(PP)
 
     return best
 
+#test
+mat1 = [[float("inf"),1,0,2,2],
+       [3,float("inf"),2,0,1],
+       [1,2,float("inf"),2,0],
+       [4,0,3,float("inf"),4],
+       [0,2,7,0,float("inf")]]
+print(np.array(mat1))
+print(little(mat1))
