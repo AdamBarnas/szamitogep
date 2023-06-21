84daafc4057859241e9a0eb99b0dc5b96dc1ed7c
AdamBarnas <101982972+AdamBarnas@users.noreply.github.com>
2023-06-21 10:06:53 +0200

    nareszcie_chyba_dziala

--- A_little.py
+++ A_little.py
@@ -26,7 +26,7 @@ def krok3(P: Podproblem):
         P.M[P.wybrane[-1][1]][P.wybrane[-1][0]] = float("inf")
         zabranianie(P.M, P.wybrane)
     else:
-        P.M[P.zabronione[-1][1]][P.zabronione[-1][0]] = float("inf")
+        P.M[P.zabronione[-1][0]][P.zabronione[-1][1]] = float("inf")
     P.M, LB = reduction(P.M)
     P.LB += LB
     return P  # zmienione parametry podproblemu
@@ -40,8 +40,8 @@ def little(M):
     odcinek1, koszt = owo.optymistyczne_wyznaczanie_odc(zredukowana)
     m1 = copy.deepcopy(zredukowana)
     m2 = copy.deepcopy(zredukowana)
-    LP.append(Podproblem(m1, LB, [odcinek1], [], True))
-    LP.append(Podproblem(m2, LB, [], [odcinek1], False))
+    LP.append(krok3(Podproblem(m1, LB, [odcinek1], [], True)))
+    LP.append(krok3(Podproblem(m2, LB, [], [odcinek1], False)))
     while len(LP) > 0:
         minlb = float('inf')
         for pp in LP:
@@ -49,7 +49,6 @@ def little(M):
                 minlb = pp.LB
                 podproblem = pp
         LP.remove(podproblem)
-        podproblem = krok3(podproblem)
         KZ = kryteria_zamykania(podproblem.M, best, podproblem.LB)
         # print()
         # print(owo.find_zeros(podproblem.M))
@@ -61,10 +60,10 @@ def little(M):
             odcinek, koszt = owo.optymistyczne_wyznaczanie_odc(podproblem.M)
             wybrane = podproblem.wybrane.copy()
             zabronione = podproblem.zabronione.copy()
-            m1 = copy.deepcopy(podproblem.M)
-            m2 = copy.deepcopy(podproblem.M)
-            LP.insert(0, Podproblem(m1, LB, wybrane + [odcinek], zabronione, True))
-            LP.append(Podproblem(m2, LB, wybrane, zabronione + [odcinek], False))
+            m3 = copy.deepcopy(podproblem.M)
+            m4 = copy.deepcopy(podproblem.M)
+            LP.append(krok3(Podproblem(m3, podproblem.LB, wybrane + [odcinek], zabronione, True)))
+            LP.append(krok3(Podproblem(m4, podproblem.LB, wybrane, zabronione + [odcinek], False)))
         elif KZ == "KZ3":
             wolnezera = owo.find_zeros(podproblem.M)
             for i in wolnezera:
@@ -74,16 +73,22 @@ def little(M):
                             podproblem.wybrane.append(i)
                         if j not in podproblem.wybrane:
                             podproblem.wybrane.append(j)
-            print()
-            print(np.array(podproblem.M))
-            print(zabranianie(podproblem.M, podproblem.wybrane))
-            print(podproblem.wybrane)
-            print(podproblem.zabronione)
-            if best > podproblem.LB:
-                best = podproblem.LB
+            if len(zabranianie(podproblem.M, podproblem.wybrane)) == len(M):
+                print("zamykamy KZ3")
+                print(np.array(podproblem.M))
+                print(zabranianie(podproblem.M, podproblem.wybrane))
+                print(podproblem.wybrane)
+                print(podproblem.zabronione)
+                if best > podproblem.LB:
+                    best = podproblem.LB
+            else:
+                print("zamykamy KZ1")
 
         elif KZ == "KZ2":
             print("zamykamy KZ2")
+            print(podproblem.LB)
+            print(podproblem.wybrane)
+            print(podproblem.zabronione)
         else:
             print("error")
             break
@@ -94,21 +99,21 @@ def little(M):
 
 
 # test
-mat1 = [[float("inf"), 1, 0, 2, 2],
-        [3, float("inf"), 2, 0, 1],
-        [1, 2, float("inf"), 2, 0],
-        [4, 0, 3, float("inf"), 4],
-        [0, 2, 7, 0, float("inf")]]
-# mat1 = np.array([
-#     [float('inf'), 1, 5, 3, 1, 4, 2, 3, 7],
-#     [2, float('inf'), 3, 3, 3, 1, 4, 1, 2],
-#     [4, 3, float('inf'), 5, 2, 4, 2, 2, 6],
-#     [6, 5, 5, float('inf'), 1, 9, 6, 1, 2],
-#     [5, 7, 1, 3, float('inf'), 8, 7, 2, 4],
-#     [4, 3, 7, 3, 9, float('inf'), 2, 6, 2],
-#     [1, 5, 6, 1, 3, 7, float('inf'), 8, 5],
-#     [7, 2, 6, 1, 6, 7, 3, float('inf'), 2],
-#     [7, 2, 6, 1, 6, 1, 3, 2, float('inf')]
-# ])
+# mat1 = [[float("inf"), 1, 5, 2, 2],
+#         [3, float("inf"), 2, 4, 3],
+#         [1, 2, float("inf"), 2, 6],
+#         [4, 7, 3, float("inf"), 4],
+#         [9, 2, 7, 2, float("inf")]]
+mat1 = np.array([
+    [float('inf'), 1, 5, 3, 1, 4, 2, 3, 7],
+    [2, float('inf'), 3, 3, 3, 1, 4, 1, 2],
+    [4, 3, float('inf'), 5, 2, 4, 2, 2, 6],
+    [6, 5, 5, float('inf'), 1, 9, 6, 1, 2],
+    [5, 7, 1, 3, float('inf'), 8, 7, 2, 4],
+    [4, 3, 7, 3, 9, float('inf'), 2, 6, 2],
+    [1, 5, 6, 1, 3, 7, float('inf'), 8, 5],
+    [7, 2, 6, 1, 6, 7, 3, float('inf'), 2],
+    [7, 2, 6, 1, 6, 1, 3, 2, float('inf')]
+])
 print(np.array(mat1))
-print(little(mat1))
+print("najlepsze rozwiazanie: ", little(mat1))
