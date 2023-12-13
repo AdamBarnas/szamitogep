36b1d03b92b2ca62238dfd96a8c6ecbd995e8a71
AdamBarnas <101982972+AdamBarnas@users.noreply.github.com>
2023-12-13 19:18:14 +0100

    drobne_poprawki

--- Zakupy_w_Auchan/struktury_danych.py
+++ Zakupy_w_Auchan/struktury_danych.py
@@ -36,6 +36,9 @@ exit_coords1 = (7, 55)
 exit_coords2 = (7, 610)
 exit_name = "EXIT"
 
+cartesian = "c"
+pitagorean = "p"
+
 # #shopping list
 # LZ = []
 
@@ -159,7 +162,7 @@ def create_ant_list(LZ: list[Product]) -> list[Ant]:
             AL.append(Ant(product))
     return AL
 
-def calculate_distance(p1: Product, p2: Product) -> float:
+def calculate_distance(p1: Product, p2: Product, method: str =cartesian) -> float:
     if p2.name == exit_name and p1.coords[1] < 55:
         coords1 = p1.coords
         coords2 = exit_coords1
@@ -173,9 +176,13 @@ def calculate_distance(p1: Product, p2: Product) -> float:
         coords1 = p1.coords
         coords2 = p2.coords
     distance = 0.0
-    for i in range(2):
-        distance += (coords1[i] - coords2[i])*(coords1[i] - coords2[i])
-    distance = np.sqrt(distance)
+    if method == cartesian:
+        for i in range(2):
+            distance += abs(coords1[i] - coords2[i])
+    else:
+        for i in range(2):
+            distance += (coords1[i] - coords2[i])*(coords1[i] - coords2[i])
+        distance = np.sqrt(distance)
     return distance
 
 def calculate_fatigue(distance, mass) -> float:
@@ -276,13 +283,7 @@ def ant_algorithm(LZ: list[Product]) -> list[Ant]:
     print(better_list)
     # print("AM:\n", AM)  
     # print("FM:\n", FM)
-<<<<<<< HEAD
-    plt.imshow(FM)
-    plt.show()
-    return AL
 
-=======
     # plt.imshow(FM)
     # plt.show()
     return best_ant.visited
->>>>>>> 822e175151302eb734130008649a97d9dcb1b233
